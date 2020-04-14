d159b83b95e9f52ba5a28b8a57e40b34a8f0e978
ha <matthias.hamann@balvi.de>
2020-04-14 10:12:04 +0100

    #1 * fix tests, add junit 5

---
+++ modules/core/test/de/balvi/ope/core/optaplanner/IntValuePlanningEntityTest.java
@@ -0,0 +1,21 @@
+package de.balvi.ope.core.optaplanner;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IntValuePlanningEntityTest {
+
+    IntValuePlanningEntity sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new IntValuePlanningEntity(1);
+    }
+
+    @Test
+    void check_IntValuePlanningEntity_is_1() {
+        assertEquals(sut.value, 1);
+    }
+}
