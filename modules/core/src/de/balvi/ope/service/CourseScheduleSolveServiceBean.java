b0d6e9015e7affc61d6e3c7f6c96320132b0504a
janaschwieger <jana.schwieger@stud.th-luebeck.de>
2020-04-16 14:09:52 +0200

    * refactoring

--- modules/core/src/de/balvi/ope/service/CourseScheduleSolveServiceBean.java
+++ modules/core/src/de/balvi/ope/service/CourseScheduleSolveServiceBean.java
@@ -12,37 +12,33 @@ import java.util.stream.Collectors;
 @Service(CourseScheduleSolveService.NAME)
 public class CourseScheduleSolveServiceBean implements CourseScheduleSolveService {
 
+    public static final String JAVA_CONFIGURATION = "de/balvi/ope/core/courseschedule/courseScheduleSolverConfiguration.xml";
+    public static final String DROOLS_CONFIGURATION = "de/balvi/ope/core/courseschedule/courseScheduleSolverWithDroolsConfiguration.xml";
+
+
     public List<String> solveCourseSchedule() {
         SolverFactory<CourseSchedule> solverFactory = SolverFactory
-                .createFromXmlResource(
-                        "de/balvi/ope/core/courseschedule/courseScheduleSolverConfiguration.xml",
-                        getClass().getClassLoader()
+                .createFromXmlResource(JAVA_CONFIGURATION, getClass().getClassLoader()
                 );
 
-        Solver<CourseSchedule> courseScheduleSolver = solverFactory.buildSolver();
-
-        CourseSchedule unsolvedCourseSchedule = getUnsolvedCourseScheduleExampleData();
-
-        CourseSchedule solvedCourseSchedule = courseScheduleSolver.solve(unsolvedCourseSchedule);
-
-        return courseScheduleToString(solvedCourseSchedule.getLectureList()) ;
+        return solveCourseScheduleFromSolverFactory(solverFactory);
     }
 
     @Override
     public List<String> solveCourseScheduleWithDrools() {
         SolverFactory<CourseSchedule> solverFactory = SolverFactory
-                .createFromXmlResource(
-                        "de/balvi/ope/core/courseschedule/courseScheduleSolverWithDroolsConfiguration.xml",
-                        getClass().getClassLoader()
+                .createFromXmlResource(DROOLS_CONFIGURATION, getClass().getClassLoader()
                 );
 
+        return solveCourseScheduleFromSolverFactory(solverFactory);
+    }
+
+    private List<String> solveCourseScheduleFromSolverFactory(SolverFactory<CourseSchedule> solverFactory) {
+
         Solver<CourseSchedule> courseScheduleSolver = solverFactory.buildSolver();
-
         CourseSchedule unsolvedCourseSchedule = getUnsolvedCourseScheduleExampleData();
-
         CourseSchedule solvedCourseSchedule = courseScheduleSolver.solve(unsolvedCourseSchedule);
-
-        return courseScheduleToString(solvedCourseSchedule.getLectureList()) ;
+        return courseScheduleToString(solvedCourseSchedule.getLectureList());
     }
 
     public List<String> courseScheduleToString(List<Lecture> solvedCourseSchedule) {
@@ -57,8 +53,8 @@ public class CourseScheduleSolveServiceBean implements CourseScheduleSolveServic
 
     /**
      * https://www.codeflow.site/de/article/opta-planner
-     *
-     * result after 30sec solving:
+     * <p>
+     * result after 30sec solving with JAVA ScoreCalculator:
      * Lecture in Room 3 during Period 2 by Teacher 1
      * Lecture in Room 2 during Period 3 by Teacher 2
      * Lecture in Room 3 during Period 1 by Teacher 3
@@ -70,9 +66,24 @@ public class CourseScheduleSolveServiceBean implements CourseScheduleSolveServic
      * Lecture in Room 1 during Period 3 by Teacher 9
      * Lecture in Room 1 during Period 1 by Teacher 10
      * Lecture in Room 2 during Period 2 by Teacher 11
-     *
+     * <p>
      * 10:23:15.145 INFO  o.o.c.i.l.DefaultLocalSearchPhase       - Local Search phase (1) ended: time spent (30005), best score (-2hard/0soft), score calculation speed (460/sec), step total (11935).
      * 10:23:15.145 INFO  o.o.core.impl.solver.DefaultSolver      - Solving ended: time spent (30005), best score (-2hard/0soft), score calculation speed (460/sec), phase total (2), environment mode (REPRODUCIBLE).
+     * <p>
+     * result after 30sec solving with drl Score Calculator:
+     * <p>
+     * 13:46:18.431 INFO  o.o.core.impl.solver.DefaultSolver      - Solving ended: time spent (30010), best score (-82hard/0soft), score calculation speed (239/sec), phase total (2), environment mode (REPRODUCIBLE).
+     * 13:46:18.459 INFO  d.balvi.ope.web.screens.SolverScreen    - Lecture in Room 1 during Period 1 by Teacher 1
+     * Lecture in Room 2 during Period 1 by Teacher 2
+     * Lecture in Room 1 during Period 2 by Teacher 3
+     * Lecture in Room 3 during Period 2 by Teacher 4
+     * Lecture in Room 3 during Period 3 by Teacher 5
+     * Lecture in Room 3 during Period 3 by Teacher 6
+     * Lecture in Room 2 during Period 2 by Teacher 7
+     * Lecture in Room 1 during Period 3 by Teacher 8
+     * Lecture in Room 1 during Period 2 by Teacher 9
+     * Lecture in Room 2 during Period 1 by Teacher 10
+     * Lecture in Room 3 during Period 1 by Teacher 11
      *
      * @return
      */
